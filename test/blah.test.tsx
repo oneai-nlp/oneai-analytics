36dc411a5d5cb7d95839de9ab4b50cd1b41df870
Ron Helfer <helferon23@gmail.com>
2022-08-25 19:48:45 +0300

    fix: added polyfills to jest

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Treemap } from '../stories/Thing.stories';
 import { render } from '@testing-library/react';
+import '../src/common/polyfill';
 
 describe('Treemap', () => {
   it('renders without crashing', () => {
