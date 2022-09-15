cd2aa1db9ea0f1fd3a0e402be7cce0d552707c49
Ron Helfer <helferon23@gmail.com>
2022-09-15 17:55:49 +0300

    fix: added exports for all types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
 import './index.css';
 import './common/utils/polyfill';
-import { OneAiAnalytics } from './components/OneAiAnalytics';
-
-export { OneAiAnalytics };
+export { OneAiAnalytics } from './components/OneAiAnalytics';
+export { OneAIAnalyticsApiWrapper } from './wrappers/OneAIAnalyticsApiWrapper';
+export { OneAIAnalyticsStaticDataWrapper } from './wrappers/OneAIAnalyticsStaticDataWrapper';
+export {
+  TreemapProps,
+  BarChartProps,
+  DataNode,
+  ExampleNode,
+  OneAIDataNode,
+  OneAiAnalyticsProps,
+  NodeType,
+  OneAIAnalyticsApiWrapperProps,
+  OneAIAnalyticsStaticDataWrapperProps,
+} from './common/types/components';
+export { ItemsDisplayComponentProps } from './common/types/configurations';
+export { Cluster, Phrase, Item } from './common/types/modals';
