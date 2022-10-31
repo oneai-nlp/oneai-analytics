42eb92f49c9fdd3d063cea549f838c88aef3efb6
Ron Helfer <helferon23@gmail.com>
2022-10-31 19:25:41 +0200

    fix: fixed test

--- test/general.test.tsx
+++ test/general.test.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import '../src/common/utils/polyfill';
-import { OneAIAnalyticsApiWrapper } from '../src/wrappers/OneAIAnalyticsApiWrapper';
+import React from 'react';
 import * as ResizeObserverModule from 'resize-observer-polyfill';
+import '../src/common/utils/polyfill';
+import { OneAiAnalytics } from '../src/components/OneAiAnalytics';
 (global as any).ResizeObserver = ResizeObserverModule.default;
 
 describe('Treemap', () => {
   it('renders without crashing', () => {
-    const component = render(<OneAIAnalyticsApiWrapper />);
+    const component = render(<OneAiAnalytics dataNodes={[]} />);
     component.unmount();
   });
 });
