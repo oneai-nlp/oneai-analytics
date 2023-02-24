227d867d25973f22277bc1e2d7a0f09bd84d8cbf
Ron Helfer <helferon23@gmail.com>
2023-02-24 10:29:03 +0200

    feat: updated design

--- src/common/components/UploadCSVComponents/constants.ts
+++ src/common/components/UploadCSVComponents/constants.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 export const IGNORE_ID = 'False';
 export const CUSTOM_VALUE_ID = 'CUSTOM_VALUE';
 
@@ -5,6 +6,11 @@ export const COLUMN_TYPES_OPTIONS = [
   { id: IGNORE_ID, label: 'ignore' },
   { id: 'input', label: 'text' },
   { id: 'input_translated', label: 'translation' },
-  { id: 'timestamp', label: 'date & time' },
+  {
+    id: 'timestamp',
+    label: 'date & time',
+    validation: (value: string) =>
+      moment(value, moment.ISO_8601, true).isValid(),
+  },
   { id: CUSTOM_VALUE_ID, label: 'custom value' },
 ];
